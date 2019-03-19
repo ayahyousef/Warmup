cf3a9a715672e1bdea659d7a0ddc51796e46c7b5
RBK-22 <rbk-22@RBK-22s-Mini.Dlink>
2019-03-19 00:49:21 -0700

    any thing

--- closestMultipleOf10.js
+++ closestMultipleOf10.js
@@ -4,5 +4,21 @@
 	// 25 ===> 30
 	// 37 ===> 40
  function closestMultipleOf10(num) {
- 	//your code is here
- }
+ 	if(num%10==0){
+
+ 		return num;
+ 	}
+else if(num%10!=0&&num%10>=5){
+return num+10-num%10;}
+
+else{
+
+return num-num%10;
+
+}
+
+
+}
+
+
+ 
